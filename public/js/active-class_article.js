6385a2d47a7608cedc9c7b97bb5849e61e9b4d69
thejaffe <jeremy.trantham@gmail.com>
2018-12-14 13:51:49 -0500

    Added comments to js scripts

--- public/js/active-class_article.js
+++ public/js/active-class_article.js
@@ -1,12 +1,12 @@
 // Add active class to links on article pages
 
 (function() {
-  let elements = document.querySelectorAll("nav a");
-      current  = window.location.pathname.split("/")[1];
+  let elements = document.querySelectorAll('nav a');
+      current  = window.location.pathname.split('/')[1];
 
   [...elements].forEach(element => {
-    if(element.pathname.split("/")[1] == current) {
-      element.classList.add("active");
+    if(element.pathname.split('/')[1] == current) {
+      element.classList.add('active');
     };
   });
 })();
--- public/js/active-class_review.js
+++ public/js/active-class_review.js
@@ -1,12 +1,12 @@
 // Add active class to links on review pages
 
 (function() {
-  let elements = document.getElementsByClassName("beer-circle"),
-      current = window.location.pathname.split("/")[1];
+  let elements = document.getElementsByClassName('beer-circle'),
+      current = window.location.pathname.split('/')[1];
 
   [...elements].forEach(element => {
-    if(element.pathname.split("/")[1] == current) {
-      element.classList.add("active");
+    if(element.pathname.split('/')[1] == current) {
+      element.classList.add('active');
     };
   });
 })();
