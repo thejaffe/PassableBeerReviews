9ceb21cded7345f57560c1f807f6bceffc0b5791
thejaffe <jeremy.trantham@gmail.com>
2019-10-11 08:50:04 -0400

    summerfest review

--- src/middleware/publishedViews.ts
+++ src/middleware/publishedViews.ts
@@ -18,6 +18,7 @@ export class PublishedViews {
       "happybirthdaynana.html"
     ];
     this.reviews = [
+      "summerfest.html",
       "swipelight.html",
       "mythos.html",
       "highlife.html",
