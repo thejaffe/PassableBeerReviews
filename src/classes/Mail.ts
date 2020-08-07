4965745879049e4c0add6ffb2e98927194c3e92e
thejaffe <jeremy.trantham@gmail.com>
2020-08-07 15:27:16 -0400

    Long overdue package updates

--- src/classes/Mail.ts
+++ src/classes/Mail.ts
@@ -1,44 +1,66 @@
 import { MailData } from "@sendgrid/helpers/classes/mail";
 import { Request } from "express";
 
-export class Mail implements MailData {    public isMultiple?: boolean;
-    public substitutionWrappers?: string[];
-    public substitutions?: { [key: string]: string; };
-    public trackingSettings?: import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/mail").TrackingSettings;
-    public mailSettings?: import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/mail").MailSettings;
-    public asm?: import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/mail").ASMOptions;
-    public customArgs?: { [key: string]: any; };
-    public category?: string;
-    public categories?: string[];
-    public headers?: { [key: string]: string; };
-    public sections?: { [key: string]: string; };
-    public batchId?: string;
-    public cc?: string | { name?: string; email: string; } | Array<import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/email-address").EmailData>;
-    public attachments?: Array<import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/attachment").AttachmentData>;
-    public personalizations?: Array<import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/personalization").PersonalizationData>;
-    public templateId?: string;
-    public content?: Array<import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/mail").MailContent>;
-    public html?: string;
-    public text?: string;
-    public subject?: string;
-    public sendAt?: number;
-    public replyTo?: import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/email-address").EmailData;
-    public from: import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/email-address").EmailData;
-    public bcc?: string | { name?: string; email: string; } | Array<import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/email-address").EmailData>;
+export class Mail implements MailData {
+  public isMultiple?: boolean;
+  public substitutionWrappers?: string[];
+  public substitutions?: { [key: string]: string };
+  public trackingSettings?: import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/mail").TrackingSettings;
+  public mailSettings?: import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/mail").MailSettings;
+  public asm?: import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/mail").ASMOptions;
+  public customArgs?: { [key: string]: any };
+  public category?: string;
+  public categories?: string[];
+  public headers?: { [key: string]: string };
+  public sections?: { [key: string]: string };
+  public batchId?: string;
+  public cc?:
+    | string
+    | { name?: string; email: string }
+    | Array<
+        import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/email-address").EmailData
+      >;
+  public attachments?: Array<
+    import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/attachment").AttachmentData
+  >;
+  public personalizations?: Array<
+    import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/personalization").PersonalizationData
+  >;
+  public templateId?: string;
+  public content?: Array<
+    import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/mail").MailContent
+  >;
+  public html?: string;
+  public text?: string;
+  public subject?: string;
+  public sendAt?: number;
+  public replyTo?: import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/email-address").EmailData;
+  public from: import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/email-address").EmailData;
+  public bcc?:
+    | string
+    | { name?: string; email: string }
+    | Array<
+        import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/email-address").EmailData
+      >;
 
-    public to?: string | { name?: string; email: string; } | Array<import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/email-address").EmailData>;    public ipPoolName?: string;
+  public to?:
+    | string
+    | { name?: string; email: string }
+    | Array<
+        import("/Users/zod/Projects/PBR/node_modules/@sendgrid/helpers/classes/email-address").EmailData
+      >;
+  public ipPoolName?: string;
 
-    // Constructor formats form request into MailData
-    constructor(req: Request) {
-      const firstName = req.body.first;
-      const lastName = req.body.last;
-      const email = req.body.email;
+  // Constructor formats form request into MailData
+  constructor(req: Request) {
+    const firstName = req.body.first;
+    const lastName = req.body.last;
+    const email = req.body.email;
 
-      this.from = "beersuggestions@passablebeers.com";
-      this.replyTo = email;
-      this.subject = `Beer Suggestion from ${ firstName + " " + lastName }`;
-      this.text = req.body.suggestion;
-      this.to = "passablebeers@gmail.com";
-    }
-
+    this.from = "beersuggestions@passablebeers.com";
+    this.replyTo = email;
+    this.subject = `Beer Suggestion from ${firstName + " " + lastName}`;
+    this.text = req.body.suggestion;
+    this.to = "passablebeers@gmail.com";
+  }
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -16,7 +16,6 @@ router.get("/", (req: Request, res: Response) => res.render("index.html"));
 
 // article routes
 router.get("/:article", (req: Request, res: Response) => {
-
   const route = publishedViews.routeGenerator(req.params.article);
 
   if (publishedViews.validArticle(route)) {
@@ -26,12 +25,10 @@ router.get("/:article", (req: Request, res: Response) => {
   } else {
     throw new Error("Cannot Get request page");
   }
-
 });
 
 // beer review routes
 router.get("/reviews/:beer", (req: Request, res: Response) => {
-
   const route = publishedViews.routeGenerator(req.params.beer);
 
   if (publishedViews.validReview(route)) {
@@ -39,36 +36,47 @@ router.get("/reviews/:beer", (req: Request, res: Response) => {
   } else {
     throw new Error("Cannot Get requested page");
   }
-
 });
 
 // send email route
-router.post("/contact", asyncWrapper(async (req: Request, res: Response) => {
+router.post(
+  "/contact",
+  asyncWrapper(async (req: Request, res: Response) => {
+    body("first")
+      .trim()
+      .escape();
+    body("last")
+      .trim()
+      .escape();
+    body("email")
+      .trim()
+      .escape();
+    body("text")
+      .trim()
+      .escape();
 
-  body("first").trim().escape();
-  body("last").trim().escape();
-  body("email").trim().escape();
-  body("text").trim().escape();
+    const msg = await new Mail(req);
+    await SendGrid.send(msg);
 
-  const msg = await new Mail(req);
-  await SendGrid.send(msg);
-
-  res.render("success.html");
-}));
+    res.render("success.html");
+  })
+);
 
 // Error routes
-router.get("*", asyncWrapper(async (req: Request, res: Response) => {
+router.get(
+  "*",
+  asyncWrapper(async (req: Request, res: Response) => {
+    await new Promise((resolve) => setTimeout(() => resolve(), 50));
+    throw new Error("404 Cannot Get requested resource");
+  })
+);
 
-  await new Promise((resolve) => setTimeout(() => resolve(), 50));
-  throw new Error("404 Cannot Get requested resource");
-
-}));
-
-router.all("*", asyncWrapper(async (req: Request, res: Response) => {
-
-  await new Promise((resolve) => setTimeout(() => resolve(), 50));
-  throw new Error("Unknown, this error has been reported");
-
-}));
+router.all(
+  "*",
+  asyncWrapper(async (req: Request, res: Response) => {
+    await new Promise((resolve) => setTimeout(() => resolve(), 50));
+    throw new Error("Unknown, this error has been reported");
+  })
+);
 
 export const Routes: Router = router;
