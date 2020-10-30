6b24a869a23d94f05d6d00744c6e0da909449979
thejaffe <jeremy.trantham@gmail.com>
2020-10-30 16:12:01 -0400

    actual port fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from "./app";
 
-const PORT: number = +process.env.PORT || 8080; // default port to listen
+const PORT = process.env.PORT || 8080; // default port to listen
 
 app.listen(PORT);
